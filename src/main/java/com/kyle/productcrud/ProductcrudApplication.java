d8a92e1dbb7d7535a2353a99edea2b520297a7fe
Kyle <kderrick1988@gmail.com>
2019-01-09 19:16:27 -0800

    initial commit

---
+++ src/main/java/com/kyle/productcrud/ProductcrudApplication.java
@@ -0,0 +1,14 @@
+package com.kyle.productcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcrudApplication.class, args);
+	}
+
+}
+
