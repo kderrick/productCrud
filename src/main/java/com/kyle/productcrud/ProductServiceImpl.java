dbc04e76dcdaadf3d635568ce1c8475bdc42a3e0
Kyle <kderrick1988@gmail.com>
2019-01-21 15:40:02 -0800

    update ProductService to include appropriate annotations

---
+++ src/main/java/com/kyle/productcrud/ProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kyle.productcrud;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import com.kyle.productcrud.entities.Product;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public List<Product> getProducts() {
+		return null;
+	}
+
+	@Override
+	public Product getProduct(int id) {
+		return null;
+	}
+
+	@Override
+	public Response createProduct(Product product) {
+		return null;
+	}
+
+	@Override
+	public Response updateProduct(Product product) {
+		return null;
+	}
+
+	@Override
+	public Response deleteProduct(int id) {
+		return null;
+	}
+
+}
